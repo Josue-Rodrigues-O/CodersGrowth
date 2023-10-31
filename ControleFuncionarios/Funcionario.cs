4aad6d354f8165f587b703ee23beb38723c18060
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware>
2023-10-31 13:18:55 -0300

    criar_modelo_de_dados alteracao na classe Funcionario mudanca dos metodos aessores

--- ControleFuncionarios/Funcionario.cs
+++ ControleFuncionarios/Funcionario.cs
@@ -2,42 +2,16 @@
 
 public class Funcionario
 {
-	private DateTime _DataNascimento;
-	private string _Nome;
-	private double _Salario;
-	private bool _EhCasado;
-	private string _Cpf;
-	private enum _Sexo : byte
+	public enum Sexo : byte
 	{
 		Masculino = 0,
 		Feminino = 1
 	}
 
-    #region Acessores
-	public DateTime DataNascimento
-	{
-		get { return _DataNascimento; }
-		set { _DataNascimento = value; }
-	}
-	public string Nome
-	{
-		get { return _Nome; }
-		set { _Nome = value; }
-	}
-	public double Salario
-	{
-		get { return _Salario; }
-		set { _Salario = value; }
-	}
-	public bool EhCasado
-	{
-		get { return _EhCasado; }
-		set { _EhCasado = value; }
-	}
-	public string Cpf
-	{
-		get { return _Cpf; }
-		set { _Cpf = value; }
-	}
-    #endregion
+    public DateTime DataNascimento { get; set; }
+    public string Nome { get; set; }
+	public double Salario { get; set;}
+	public bool EhCasado { get; set; }
+	public string Cpf { get; set; }
+	public int Id { get; set; }
 }
