f25c0020f97533bfb622a225dd6762d8547487ec
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware>
2023-10-31 13:47:22 -0300

    criar_modelo_de_dados alteracao na ordem dos acessores

--- ControleFuncionarios/Funcionario.cs
+++ ControleFuncionarios/Funcionario.cs
@@ -2,15 +2,16 @@
 
 public class Funcionario
 {
-	public enum Sexo : byte
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public double Salario { get; set; }
+    public bool EhCasado { get; set; }
+    public DateTime DataNascimento { get; set; }
+
+    public enum Sexo : byte
 	{
 		Masculino = 0,
 		Feminino = 1
 	}
-    public DateTime DataNascimento { get; set; }
-    public string Nome { get; set; }
-	public double Salario { get; set;}
-	public bool EhCasado { get; set; }
-	public string Cpf { get; set; }
-	public int Id { get; set; }
 }
