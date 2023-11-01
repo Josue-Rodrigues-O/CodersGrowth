fc728dd27fac8761a0ac72ddb9111cdb0e265845
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2023-11-01 11:12:50 -0300

    criacao de 2 atributos na classe Funcionario + implementacao da lista e criacao da tela que exibe a lista

--- ControleFuncionarios/Form1.Designer.cs
+++ ControleFuncionarios/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGrid = new DataGridView();
+            btnAdicionar = new Button();
+            btnEditar = new Button();
+            btnRemover = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            dataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGrid.Location = new Point(12, 12);
+            dataGrid.Name = "dataGrid";
+            dataGrid.RowTemplate.Height = 25;
+            dataGrid.Size = new Size(776, 349);
+            dataGrid.TabIndex = 0;
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.Location = new Point(551, 404);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Size = new Size(75, 23);
+            btnAdicionar.TabIndex = 1;
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(632, 404);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(75, 23);
+            btnEditar.TabIndex = 2;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            btnRemover.Location = new Point(713, 404);
+            btnRemover.Name = "btnRemover";
+            btnRemover.Size = new Size(75, 23);
+            btnRemover.TabIndex = 3;
+            btnRemover.Text = "Remover";
+            btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRemover);
+            Controls.Add(btnEditar);
+            Controls.Add(btnAdicionar);
+            Controls.Add(dataGrid);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGrid).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGrid;
+        private Button btnAdicionar;
+        private Button btnEditar;
+        private Button btnRemover;
     }
 }
--- ControleFuncionarios/Form1.cs
+++ ControleFuncionarios/Form1.cs
@@ -1,10 +1,51 @@
+using System.Globalization;
+
 namespace ControleFuncionarios
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
+            CultureInfo ci = CultureInfo.InvariantCulture;
             InitializeComponent();
+            AtualizarGrid();
+            dataGrid.ReadOnly = true;
+        }
+
+        private List<Funcionario> Lista()
+        {
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            funcionarios.Add(new Funcionario()
+            {
+                Id = 1,
+                Nome = "AAAA",
+                Cpf = "000.000.000-00",
+                Telefone = "(00) 0 0000-0000",
+                Salario = 1000.50M,
+                EhCasado = true,
+                DataNascimento = new DateTime(2003, 10, 12),
+                genero = Funcionario.Sexo.Feminino,
+            });
+
+            funcionarios.Add(new Funcionario()
+            {
+                Id = 2,
+                Nome = "antonio",
+                Cpf = "111.111.111-111",
+                Telefone = "(11) 1 1111-1111",
+                Salario = 2000.00M,
+                EhCasado = false,
+                DataNascimento = new DateTime(1998, 05, 09),
+                genero = Funcionario.Sexo.Masculino
+            });
+
+            return funcionarios;
+        }
+
+        private void AtualizarGrid()
+        {
+            dataGrid.DataSource = Lista();
         }
     }
 }
