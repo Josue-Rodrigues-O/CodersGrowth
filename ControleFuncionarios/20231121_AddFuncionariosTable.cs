a74b00bc9a6bf3225900a80d79a6e51c1ce3ef12
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2023-11-21 13:53:04 -0300

    task pronta mas falta entender melhor como funciona

---
+++ ControleFuncionarios/20231121_AddFuncionariosTable.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFuncionarios
+{
+    [Migration(20231121103000)]
+    public class AddFuncionariosTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("TabFuncionarios")
+                .WithColumn("Id").AsInt64().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString().NotNullable()
+                .WithColumn("Cpf").AsString().NotNullable()
+                .WithColumn("Telefone").AsString().NotNullable()
+                .WithColumn("Salario").AsDecimal().NotNullable()
+                .WithColumn("EhCasado").AsBoolean().NotNullable()
+                .WithColumn("DataNascimeno").AsDate().NotNullable()
+                .WithColumn("Genero").AsString().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("TabFuncionarios");
+        }
+    }
+}
