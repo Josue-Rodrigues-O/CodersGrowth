771a903b04987cbb2dc9f14e039218cffff62a58
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2023-12-14 08:59:16 -0300

    alterações na program

--- InterfaceUsuarioSAPUI5/Program.cs
+++ InterfaceUsuarioSAPUI5/Program.cs
@@ -1,17 +1,31 @@
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseStaticFiles();
-}
-
 app.UseHttpsRedirection();
 
+app.UseDefaultFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")
+),
+
+    ContentTypeProvider = new FileExtensionContentTypeProvider
+    {
+        Mappings = { [".properties"] = "application/x-msdownload" }
+    }
+});
 app.UseAuthorization();
 
 app.MapControllers();
