d70e60d8d1bfec15e227212a51b61072d967c519
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2023-12-18 09:51:03 -0300

    Commit feito antes de conecetar com o banco de dados

--- InterfaceUsuarioSAPUI5/wwwroot/Component.js
+++ InterfaceUsuarioSAPUI5/wwwroot/Component.js
@@ -1,25 +1,19 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/resource/ResourceModel"
-], (UIComponent,ResourceModel) => {
-    'use strict';
+], (UIComponent, ResourceModel) => {
+    "use strict";
 
     return UIComponent.extend("controle.funcionarios.Component", {
         metadata: {
-            "interfaces": ["sap.ui.IAsyncContentCreation"],
-            "rootView": {
-                "viewName": "controle.funcionarios.view.App",
-                "type": "XML",
-                "id": "app"
-            }
+            interfaces: ["sap.ui.core.IAsyncContentCreation"],
+            manifest: "json"
         },
-        onInit() {
+
+        init() {
             UIComponent.prototype.init.apply(this, arguments);
 
-            const i18nModel = new ResourceModel({
-                bundleName: "controle.funcionarios.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
+           this.getRouter().initialize();
         }
     });
 });
--- InterfaceUsuarioSAPUI5/wwwroot/Controller/App.controller.js
+++ InterfaceUsuarioSAPUI5/wwwroot/Controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-], (Controller, MessageToast)  => {
+], (Controller, MessageToast) => {
     "use strict";
-    
+
     return Controller.extend("controle.funcionarios.controller.App", {
-        onMostrarOla(){
-            MessageToast.show("OLAAAAAA");
+        onMostrarOla() {
+            // show message
+            MessageToast.show("AOOOOOO POTENCIA");
         }
     });
 });
