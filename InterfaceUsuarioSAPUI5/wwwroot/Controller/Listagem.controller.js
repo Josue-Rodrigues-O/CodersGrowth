82d32359a90bb58a8229f8d2ce2895eb28d70d4c
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2023-12-19 10:41:53 -0300

    primeiro prototipo da tabela

--- InterfaceUsuarioSAPUI5/wwwroot/Controller/Listagem.controller.js
+++ InterfaceUsuarioSAPUI5/wwwroot/Controller/Listagem.controller.js
@@ -1,11 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function (Controller) {
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
-    return Controller.extend("controle.funcionarios.controller.Listagem", {
+    return Controller.extend("controle.funcionarios.Controller.Listagem", {
 
         onInit: function () {
+
+            this.ObterTodos();
+        },
+        ObterTodos(){
+            const url = "/api/Funcionario"
+            fetch(url)
+            .then(jogos => jogos.json())
+            .then(jogos => this.getView().setModel(new JSONModel(jogos), "tabelinha"))
+            .catch((erro)=>console.log(erro.error))
         }
     });
 });
