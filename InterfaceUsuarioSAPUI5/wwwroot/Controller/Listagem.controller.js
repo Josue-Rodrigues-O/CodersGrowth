7d34288115dbc7c041f6fbbc949507febb36081a
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2023-12-19 09:52:19 -0300

    Interface ok - proxima etapa é ligar ao BD

--- InterfaceUsuarioSAPUI5/wwwroot/Controller/Listagem.controller.js
+++ InterfaceUsuarioSAPUI5/wwwroot/Controller/Listagem.controller.js
@@ -1,14 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+], function (Controller) {
     "use strict";
 
-    return Controller.extend("sap.ui.webc.main.sample.Table.C", {
+    return Controller.extend("controle.funcionarios.controller.Listagem", {
 
         onInit: function () {
-            var oModel = new JSONModel(sap.ui.require.toUrl("controle/funcionarios/Repositorios/funcio.json"));
-            this.getView().setModel(oModel);
         }
     });
 });
