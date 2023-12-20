f153b42ae2ae3eabd7c4a78f89c3225b733e1fbc
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2023-12-20 14:02:42 -0300

    Commit fo dia

--- InterfaceUsuarioSAPUI5/wwwroot/Repositorios/RepositorioAPI.js
+++ InterfaceUsuarioSAPUI5/wwwroot/Repositorios/RepositorioAPI.js
@@ -4,11 +4,18 @@ sap.ui.define([
   "use strict";
 
   return{
-    obterTodos(view) {
+    obterTodos(controller) {
       const url = "/api/Funcionario"
       fetch(url)
         .then(funcionarios => funcionarios.json())
-        .then(funcionarios => view.getView().setModel(new JSONModel(funcionarios), "TabelaFuncionarios"))
+        .then(funcionarios => controller.getView().setModel(new JSONModel(funcionarios), "TabelaFuncionarios"))
+        .catch((erro) => console.log(erro.error))
+    },
+    obterPorId(controller, id){
+      const url = `/api/Funcionario/${id}`
+      fetch(url)
+        .then(funcionarios => funcionarios.json())
+        .then(funcionarios => controller.getView().setModel(new JSONModel([funcionarios]), "TabelaFuncionarios"))
         .catch((erro) => console.log(erro.error))
     }
   }
